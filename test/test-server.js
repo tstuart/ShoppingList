609a12a0da8d9b17ebf3a336213583ac53ac26c9
tstuart <tstuart@beckshybrids.com>
2015-05-08 10:43:00 -0400

    initial testing describe block.

--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,10 @@
 /**
  * Created by tstuart on 5/8/2015.
  */
+
+describe('Shopping List', function() {
+  it('should list items on get');
+  it('should add an item on post');
+  it('should edit an item on put');
+  it('should delete an item on delete');
+});
